4692698ff9578b6fdc1f33ba732562c9b9909a13
Eugene Prikazchikov <eprikazc@gmail.com>
2016-11-06 12:29:01 +0300

    Add methods to download zipped and unzipped files

--- core/views.py
+++ core/views.py
@@ -26,3 +26,28 @@ class FileRecordViewSet(
         instance = serializer.save()
         instance.filename = serializer.initial_data['uploaded_file'].name
         instance.zip_upload()
+        instance.save()
+
+    @decorators.detail_route(methods=['get'])
+    def zipped(self, request, pk):
+        obj = self.get_object()
+        return self._serve(obj.zipped_file.path, obj.filename + '.zip')
+
+    @decorators.detail_route(methods=['get'])
+    def unzipped(self, request, pk):
+        obj = self.get_object()
+        return self._serve(obj.uploaded_file.path, obj.filename)
+
+    def _serve(self, full_path, file_name):
+        # Based on django.views.static.static
+        statobj = os.stat(full_path)
+        content_type, encoding = mimetypes.guess_type(full_path)
+        content_type = content_type or 'application/octet-stream'
+        response = FileResponse(open(full_path, 'rb'), content_type=content_type)
+        response['Last-Modified'] = http_date(statobj.st_mtime)
+        if stat.S_ISREG(statobj.st_mode):
+            response['Content-Length'] = statobj.st_size
+        if encoding:
+            response['Content-Encoding'] = encoding
+        response['Content-Disposition'] = 'inline; filename="%s"' % file_name
+        return response
