273bdecb1055b2773df0707a77291d462c11277d
Eugene Prikazchikov <eprikazc@gmail.com>
2016-11-06 15:40:56 +0300

    Add docstring to zip_upload function

--- core/zip_utils.py
+++ core/zip_utils.py
@@ -10,9 +10,12 @@ from django.core.files import File
 
 @background(schedule=1)
 def zip_upload(file_record_id):
+    """
+    Takes id of file record in db, reads uploaded file and ZIPs it
+    """
     obj = FileRecord.objects.get(id=file_record_id)
     source_file_name = os.path.basename(obj.uploaded_file.path)
-    zipped_upload_file_name = prepare_zip_file(obj.uploaded_file.path)
+    zipped_upload_file_name = _prepare_zip_file(obj.uploaded_file.path)
     with zipped_upload_file_name:
         obj.zipped_file.save(
             source_file_name + '.zip',
@@ -20,7 +23,7 @@ def zip_upload(file_record_id):
     obj.save()
 
 
-def prepare_zip_file(source_file_name):
+def _prepare_zip_file(source_file_name):
     target_file_name = NamedTemporaryFile()
     with ZipFile(target_file_name, 'w') as my_zipfile:
         my_zipfile.write(
