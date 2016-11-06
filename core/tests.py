43d1c285d2a2f873f90dff14a1f48cb06e83dc62
Eugene Prikazchikov <eprikazc@gmail.com>
2016-11-06 13:55:00 +0300

    Add unit tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,53 @@
+from django.core.files.base import ContentFile
 from django.test import TestCase
 
-# Create your tests here.
+from core.models import FileRecord
+
+
+class UploadTestCase(TestCase):
+    def setUp(self):
+        self.obj = FileRecord(
+            username='Bob',
+            filename='1.txt',
+            uploaded_file=ContentFile('this is content', '1.txt')
+        )
+        self.obj.save()
+
+    def assertHeader(self, response, header_name, value):
+        self.assertEqual(
+            response._headers[header_name.lower()],
+            (header_name, value))
+
+    def test_listing(self):
+        res = self.client.get('/api/uploads/')
+        self.assertEqual(
+            res.json(),
+            [{'username': 'Bob', 'filename': '1.txt', 'id': 1}])
+
+    def test_zipped_error(self):
+        res = self.client.get('/api/uploads/1/zipped/')
+        self.assertEqual(
+            res.json(),
+            {'message': 'Archive is not ready yet'})
+
+    def test_zipped_ok(self):
+        self.obj.zip_upload()
+        res = self.client.get('/api/uploads/1/zipped/')
+        self.assertEqual(res.status_code, 200)
+        self.assertHeader(res, 'Content-Type', 'application/zip')
+        self.assertHeader(res, 'Content-Disposition', 'inline; filename="1.txt.zip"')
+
+    def test_upload(self):
+        with open('example/test_upload.txt') as fp:
+            res = self.client.post('/api/uploads/', {'username': 'Ann', 'uploaded_file': fp})
+        self.assertEqual(
+            res.json(),
+            {'id': 2, 'username': 'Ann'}
+                )
+        res = self.client.get('/api/uploads/')
+        self.assertEqual(
+            res.json(),
+            [
+                {'username': 'Bob', 'filename': '1.txt', 'id': 1},
+                {'filename': 'test_upload.txt', 'id': 2, 'username': 'Ann'},
+            ])
